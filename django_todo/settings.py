beb44191bf96377b0f8f1ac86942072d302e0d18
Deirdre McCarthy <deirdremc1968@gmail.com>
2023-09-20 22:26:46 +0000

    Hide database URL

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
 from pathlib import Path
+import os
 import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,8 +85,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 #    }
 # }
 
-DATABASES = {'default': dj_database_url.parse('postgres://zpwfpcmo:9sCG8eQm3HmAZ69Wc_8ljZxPneI5bQ4C@tai.db.elephantsql.com/zpwfpcmo')
-    }
+DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
